4b4e82f2d2ebd71ec79e845488ff1134092d62b7
virtual-frame-7789 <mttwright@gmail.com>
2019-09-21 17:36:36 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,33 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |name, index|
+    index += 1
+    puts "#{index}." + " " + name
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |element|
+    element.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  counter = 0
+  while counter < array.length
+    if cheese_types.include?(array[counter])
+      return array[counter]
+    else
+      counter += 1
+    end
+  end
+  nil
 end
+
+
